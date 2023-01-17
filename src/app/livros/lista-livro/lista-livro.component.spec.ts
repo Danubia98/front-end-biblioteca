0f6d7119dc798d461ec239909871ca2e1f57e405
Danubia de Sobral Lima <danubia.lima@gpnet.com.br>
2023-01-17 14:04:34 -0300

    criando componentes de livros

---
+++ src/app/livros/lista-livro/lista-livro.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaLivroComponent } from './lista-livro.component';
+
+describe('ListaLivroComponent', () => {
+  let component: ListaLivroComponent;
+  let fixture: ComponentFixture<ListaLivroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaLivroComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaLivroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
