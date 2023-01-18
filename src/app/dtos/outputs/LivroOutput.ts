1737fc4c7ebd6c647f8a41c9d62b2b621311519b
Danubia de Sobral Lima <danubia.lima@gpnet.com.br>
2023-01-18 14:21:54 -0300

    criando modal de exclusão do livro

---
+++ src/app/dtos/outputs/LivroOutput.ts
@@ -0,0 +1,5 @@
+export interface LivroOutput{
+    id: number
+    titulo: string
+    anoLancamento: number
+}
--- src/app/livros/lista-livro/lista-livro.component.ts
+++ src/app/livros/lista-livro/lista-livro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LivroOutput } from 'src/app/dtos/outputs/LivroOutput';
 
 @Component({
   selector: 'app-lista-livro',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-livro.component.css']
 })
 export class ListaLivroComponent implements OnInit {
+ 
+  nomeLivroParaExclusao: string = '';
+  livroParaExclusao?: LivroOutput
+
+  livros: LivroOutput[] = [
+    {id: 1, titulo:"Livro 1", anoLancamento: 2023},
+    {id: 2, titulo:"Livro 2", anoLancamento: 2005},
+    {id: 3, titulo:"Livro 3", anoLancamento: 2016},
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  abreModalParaExclusao(livro: LivroOutput){
+    this.nomeLivroParaExclusao = livro.titulo
+    this.livroParaExclusao = livro;
+  }
+
+  removeLivro(){
+  console.log(`vou remover o livro ${this.livroParaExclusao?.titulo} com id: ${this.livroParaExclusao?.id}`);
+  document.getElementById('modal-remover-livro-botao-fechar')?.click();
+  }
 }
